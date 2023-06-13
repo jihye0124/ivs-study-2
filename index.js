e1c3a741acfaa60557a5186b1e8f998fabd987d2
jihye0124 <113334775+jihye0124@users.noreply.github.com>
2023-06-13 11:34:40 +0900

    Update index.js
    import -> require 문

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-import App from './App.js';
+const App = require('./App.js');
 
 ReactDOM.render(<App />, document.getElementById('root'));
