191dbf5e3e6fd2c629800120df6327e5f6f48511
jihye0124 <113334775+jihye0124@users.noreply.github.com>
2023-06-13 10:44:14 +0900

    Delete Button.js

--- Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'https://cdn.skypack.dev/react';
-
-export default function Button({ onClick, children }) {
-    return (
-        <button className="button" onClick={onClick} style={{ margin: 'auto', width: '100%' }} id="join-button">
-            {children}
-        </button>
-    );
-}
